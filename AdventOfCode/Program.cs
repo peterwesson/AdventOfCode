07f6ca331f8f64cb061d57cd18601aca06548000
Peter <peterwesson@live.co.uk>
2017-12-18 19:00:39 +0000

    Found my day 9 solution

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -12,19 +12,20 @@
         {
             List<(IDay Day, string Input)> days = new List<(IDay Day, string Input)>
             {
-                (new Day1(), "Day1.txt"),
-                (new Day2(), "Day2.txt"),
-                (new Day4(), "Day4.txt"),
-                (new Day5(), "Day5.txt"),
-                (new Day10(), "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"),
-                (new Day11(), "Day11.txt"),
-                (new Day12(), "Day12.txt"),
-                (new Day13(), "Day13.txt"),
-                (new Day14(), "wenycdww"),
-                (new Day15(), null),
-                (new Day16(), "Day16.txt"),
-                (new Day17(), "369"),
-                (new Day18(), "Day18.txt")
+                //(new Day1(), "Day1.txt"),
+                //(new Day2(), "Day2.txt"),
+                //(new Day4(), "Day4.txt"),
+                //(new Day5(), "Day5.txt"),
+                (new Day9(), "Day9.txt"),
+                //(new Day10(), "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"),
+                //(new Day11(), "Day11.txt"),
+                //(new Day12(), "Day12.txt"),
+                //(new Day13(), "Day13.txt"),
+                //(new Day14(), "wenycdww"),
+                //(new Day15(), null),
+                //(new Day16(), "Day16.txt"),
+                //(new Day17(), "369"),
+                //(new Day18(), "Day18.txt")
             };
 
 
